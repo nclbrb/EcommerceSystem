b0c77aedf2102c3711d413ec7b908b0625330387
Nicole Borabo <borabonicole19@gmail.com>
2025-03-23 15:50:33 +0800

    Creation and fixing models

---
+++ ecommerce_project/app/Models/Order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    protected $fillable = ['user_id', 'total_price', 'status', 'checkout_date'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderDetails(): HasMany
+    {
+        return $this->hasMany(OrderDetail::class, 'order_id');
+    }
+}
