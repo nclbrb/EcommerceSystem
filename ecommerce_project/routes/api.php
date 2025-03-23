6540376ca93c250994cbbad68afe7fb48d91b8ba
Nicole Borabo <borabonicole19@gmail.com>
2025-03-23 15:51:22 +0800

    Adding api routes for order routes

--- ecommerce_project/routes/api.php
+++ ecommerce_project/routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
 
-
-// Registers API endpoints for products (CRUD)
+// Product Routes (CRUD)
 Route::apiResource('products', ProductController::class);
+
+// Order Routes
+Route::get('orders', [OrderController::class, 'index']); // List all orders
+Route::get('orders/{order}', [OrderController::class, 'show']); // View order details
+Route::get('orders/filter/{date}', [OrderController::class, 'filterByDate']); // Filter orders by date
